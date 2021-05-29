e221f8c322b9515051e7e3b0414951f6aa11decb
Andrei Lazar <andrykxd@gmail.com>
2021-05-29 17:54:32 +0200

    some fix

--- src/main/java/com/kodikasgroup/model/Vaccine.java
+++ src/main/java/com/kodikasgroup/model/Vaccine.java
@@ -1,44 +1,16 @@
 package com.kodikasgroup.model;
 
-<<<<<<< HEAD
-import com.fasterxml.jackson.annotation.JsonIgnore;
-=======
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
->>>>>>> Development
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-<<<<<<< HEAD
-import java.util.HashSet;
-import java.util.Set;
-
-=======
->>>>>>> Development
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-<<<<<<< HEAD
-public class Vaccine {
-
-    private long vaccineID;
-    private String vaccineName;
-    private Long quantity;
-
-    private Set<Availability> availabilities = new HashSet<>();
-
-
-    public Vaccine(String vaccineName, long quantity) {
-        this.vaccineName = vaccineName;
-        this.quantity = quantity;
-    }
-
-    public Vaccine(long vaccineID, String vaccineName, long quantity) {
-        this.vaccineID = vaccineID;
-=======
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Vaccine {
     private long vaccineID;
@@ -48,7 +20,6 @@ public class Vaccine {
 //    private Set<Entitled> entitleds = new HashSet<>();
 
     public Vaccine(String vaccineName, Long quantity) {
->>>>>>> Development
         this.vaccineName = vaccineName;
         this.quantity = quantity;
     }
